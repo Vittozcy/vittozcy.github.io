213395d1e1dbf72cb347e46396c5269932993ec9
Vittozcy <vittozcyprien@gmail.com>
2024-08-12 18:46:53 +0200

    mushroom v1.2

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,7 +19,7 @@
 			default:   ['1681px',   null       ],
 			xlarge:    ['1281px',   '1680px'   ],
 			large:     ['981px',    '1280px'   ],
-			medium:    ['737px',    '980px'    ],
+			medium:    ['737px',    '1100px'    ],
 			small:     ['481px',    '736px'    ],
 			xsmall:    ['361px',    '480px'    ],
 			xxsmall:   [null,       '360px'    ]
